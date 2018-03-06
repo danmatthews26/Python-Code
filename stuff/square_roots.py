7b35b1ae77548331de04750a8767d41fa5a0e9dd
Daniel Matthews <matthewsdan629@gmail.com>
2018-03-06 14:58:23 -0500

    finds square roots

--- stuff/square_roots.py
+++ stuff/square_roots.py
@@ -3,7 +3,7 @@ def square_root(perfect_square):
     ans = 0     #variable to increment  
     while (ans * ans <= perfect_square and ans * ans != perfect_square): #while the sqaure of ans is < and != equal to perfect_square
         ans = ans + 1   #increment ans so it continues to test values
-    if (ans * ans == perfect_square):   #if a perfect square has been found
+    if (ans * ans == perfect_square):   #if the value of ans is the square root of the given int
         print("square root is {}".format(ans))  #prints what the answer is 
     else:
         print("Not a perfect square")    
