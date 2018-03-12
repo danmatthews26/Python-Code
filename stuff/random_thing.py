df63b0a1ff82c68f2ae0144b035d4efda9100a21
Daniel Matthews <matthewsdan629@gmail.com>
2018-03-12 17:51:57 -0400

    lol

---
+++ stuff/random_thing.py
@@ -0,0 +1,12 @@
+#program that draws a box with |,-,_
+
+width = int(input("What will be the width? ")) #width of drawing
+height = int(input("What will be the height? ")) #height of drawing
+
+def draw_box(w,h): #draws the box (width, height) 
+    print("- " * w)
+    for i in range(0, h):
+        print("|" + "  " * (w - 1) + "|")
+    print("- " * w)    
+
+draw_box(width,height)    
